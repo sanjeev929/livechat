f6a84b3f1f071308284593384a5aba3408b972a1
Sanjeev <123805942+sanjeev929@users.noreply.github.com>
2024-03-03 06:20:15 +0530

    slive

--- livechat/livechatapp/urls.py
+++ livechat/livechatapp/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns=[
-    path("",views.register,name="register"),
-    path("home/",views.home,name="home")
+    path("",views.home,name="home"),
+    path("login/",views.login,name="login"),
+    path("register/",views.register,name="register"),
 ]
--- livechat/livechatapp/views.py
+++ livechat/livechatapp/views.py
@@ -8,15 +8,43 @@ def register(request):
         email = request.POST['email']
         password = request.POST['password']
         confirm_password = request.POST['confirm_password']
+        
         if password != confirm_password:
             return render(request, 'register.html', {'error_message': 'Passwords do not match'})
+        
         if User.objects.filter(email=email).exists():
-            return render(request, 'register.html', {'error_message': 'email is already register'})
-        user = User.objects.create_user(username=email, password=password)
-        login(request, user)
-        return redirect('home')
+            return render(request, 'register.html', {'error_message': 'Email is already registered'})
+        existing_user = User.objects.filter(username=username).exists()
+        if existing_user:
+            return render(request, 'register.html', {'error_message': 'Email is already taken'})
+        user = User.objects.create_user(email=email, username=username, password=password)
+        return redirect('login')
 
     return render(request, 'register.html')
 
+def login(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        state=False
+        user = authenticate(request, username=username, password=password)
+        print(username,password,user)
+        if user is not None:
+            state=True
+            email = user.email
+            print(email)
+            response = redirect(home)
+            response.set_cookie('state', state)
+            response.set_cookie('user', user)
+            return response
+        else:
+            return render(request, 'login.html', {'error_message': 'Invalid username or password'})
+    return render(request, 'login.html')
+
 def home(request):
-    return render(request,"home.html")
+    state_cookie = request.COOKIES.get('state')
+    user_cookie = request.COOKIES.get('user')
+    if state_cookie and user_cookie:
+        return render(request,"home.html")
+    else:
+        return render(request,"login.html")
